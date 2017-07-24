ae37df454fc14a58e7af5aad1eea34332fb7bdc8
Ibrahim Rasel <md.ibrahim.rasel@gmail.com>
2017-07-24 14:01:12 +0600

    AJAX to load all notes of a section, loads all notes, but pressing these dynamic contents returns/redirects instead of AJAX call

--- notes/static/js/script.js
+++ notes/static/js/script.js
@@ -31,6 +31,7 @@ CKEDITOR.replace('note_body', {
 
 /* AJAX to load note without completely refreshing page */
 $(document).ready(function(){
+    // $('.all_notes_class').on('click', '.single_notes', function (event) {
     $('.single_notes').on('click', function (event) {
         event.preventDefault();
         var note_id = $(this).attr('id');
@@ -60,10 +61,32 @@ $(document).ready(function(){
         event.preventDefault();
         var section_id = $(this).attr('id');
         url = "/only_section/" + section_id;
+        // gets all notes of a section and latest modified note
         req = $.ajax({
             url: url,
             method: 'GET'
         });
-        console.log(req);
+        req.done(function () {
+            // load latest modified note or in case of empty show nothing
+            var title = req.responseJSON['title'];
+            var note_body = req.responseJSON['note_body'];
+            var note_creation_date = req.responseJSON['note_creation_date'];
+            var note_modification_date = req.responseJSON['note_modification_date'];
+            $('#note_title').val(title);
+            $('#note_creation_date').val(note_creation_date[0]);
+            $('#note_modification_date').val(note_modification_date[0]);
+            CKEDITOR.instances.note_body.setData(note_body);
+            // console.log(req.responseJSON["all_notes"][0]["title"]);
+            // clear all notes from previous section
+            $('.all_notes_class').empty();
+            var all_notes = req.responseJSON["all_notes"]
+            // if section has any note, then load them
+            if (all_notes.length > 0) {
+                $(all_notes).each(function () {
+                    $('.all_notes_class').append("<div class='hover_choice single_notes' id=" + this['id'] + "><a href=/note/" + this['id'] + "><p class='note_title_gap'><strong>" + this['title'] + "</strong></p><p class='note_preview_gap horizontal_line'>" + this['preview'] + "</p></a></div>");
+                });
+            }
+
+        });
 	});
 });
